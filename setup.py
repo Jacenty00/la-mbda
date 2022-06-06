8c7a081c3c01f64f7b50c4c301129841f722ec8c
Jacek Kmiecik <41898845+Jacenty00@users.noreply.github.com>
2022-06-06 22:53:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
         'numpy==1.19.2',
         'oauthlib==3.1.0',
         'opt-einsum==3.3.0',
-        'Pillow==7.2.0',
+        'Pillow==8.3.2',
         'proglog==0.1.9',
         'protobuf==3.15.5',
         'pyasn1==0.4.8',
